81ce12c8703ae4bdcec5e73b7f83a3d582f8fff2
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2024-11-22 23:14:00 -0600

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -5,10 +5,12 @@ import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { Textarea } from "@/components/ui/textarea"
 import { Label } from "@/components/ui/label"
-import { Plus } from "lucide-react"
+import { Plus, XCircle } from "lucide-react"
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
 import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 
 interface IssueForm {
   title: string;
@@ -19,12 +21,29 @@ interface IssueForm {
 const NewIssuePage = () => {
  const router = useRouter()
   const {register, handleSubmit} = useForm<IssueForm>();
- 
+ const [error, setError] = useState('');
 
   return (
-        <form onSubmit={ handleSubmit( async (data) => { await axios.post('/api/issues', data)
-        router.push('/issues')
+        <form onSubmit={ handleSubmit( async (data) => { 
+          try {
+            await axios.post('/api/issues', data)
+            router.push('/issues')
+          } catch (error) {
+            setError(error)
+            console.log(error)
+          }
+        
         })}>
+          {error ?    
+          <Alert variant="destructive" className="mb-4">
+            <XCircle className="h-4 w-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>
+              {typeof error === 'string' 
+                ? error 
+                : 'Something went wrong. Please try again.'}
+            </AlertDescription>
+    </Alert> : ''}
           <Card className="max-w-[400px]">
             <CardContent>
               <CardTitle className="text-xl py-4">New Issue</CardTitle>
